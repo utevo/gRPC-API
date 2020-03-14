74f49d044722d1ba8a45a8c9071bc7250609e290
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-14 20:56:54 +0100

    Refactoring of server

--- server/main.go
+++ server/main.go
@@ -14,17 +14,13 @@ type server struct{}
 
 func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	a, b := request.GetA(), request.GetB()
-
 	result := a + b
-
 	return &proto.Response{Result: result}, nil
 }
 
 func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	a, b := request.GetA(), request.GetB()
-
 	result := a * b
-
 	return &proto.Response{Result: result}, nil
 }
 
