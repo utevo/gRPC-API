d29d2d48a08a3511041e6cc80ebd835fd6cc00fd
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-14 20:48:57 +0100

    Implement Service methods (Add, Multiple) for server

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	context "context"
+
+	proto "github.com/utevo/gRPC-API/proto"
+)
+
+type server struct{}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
